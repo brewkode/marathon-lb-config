5843ad0a6b167dac5d7c44bed90d857750b8be64
Sriram <sriram@indix.com>
2015-11-28 11:51:18 +0530

    MarathonTaskInfo exposes ipaddr, used in haproxy template

--- app/marathon.py
+++ app/marathon.py
@@ -1,10 +1,16 @@
+import socket
+
 class MarathonTaskInfo:
 	def __init__(self, host, ports):
 		self.host = host
 		self.ports = ports
+		self.ipaddr = socket.gethostbyname(self.host)
 
 	def http_uri(self):
 		return "http://"+self.host+":"+str(self.ports[0])
 
 	def name(self):
 		return self.host+":"+str(self.ports[0])
+
+	def resolved_name(self):
+		return self.ipaddr+":"+str(self.ports[0])
